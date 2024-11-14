3c2f79e5ea14462e00fe9059d7c0903c8044cf88
DanWin85 <dan.wingate85@gmail.com>
2024-11-14 16:10:34 +1300

    changes

--- app/src/main/java/com/example/storemanagementapp_2008043_danielwingate/AddCustomerActivity.java
+++ app/src/main/java/com/example/storemanagementapp_2008043_danielwingate/AddCustomerActivity.java
@@ -1,6 +1,8 @@
 package com.example.storemanagementapp_2008043_danielwingate;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
 public class AddCustomerActivity extends AppCompatActivity {
+
+    private EditText etFirstName, etLastName, etBirthDate, etAddress, etSuburb, etCity, etZIP;
+    private MaterialButton btnCancel, btnAddRecord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_customer);
+
+        etFirstName = findViewById(R.id.etFirstName);
+        etLastName = findViewById(R.id.etLastName);
+        etBirthDate = findViewById(R.id.etBirthDate);
+        etAddress = findViewById(R.id.etAddress);
+        etSuburb = findViewById(R.id.etSuburb);
+        etCity = findViewById(R.id.etCity);
+        etZIP = findViewById(R.id.etZip);
+        btnCancel = findViewById(R.id.btnCancel);
+        btnAddRecord = findViewById(R.id.btnAddRecord);
 
     }
 }
